2cfa9f1bb9254a42e5dee35e2425df379cc1526e
risin42 <5331402+risin42@users.noreply.github.com>
2025-01-26 14:31:00 +0900

    remove signature check

--- TMessagesProj/jni/jni.c
+++ TMessagesProj/jni/jni.c
@@ -9,7 +9,6 @@
 #include <unistd.h>
 #include <dirent.h>
 #include <sys/stat.h>
-#include "integrity.h"
 
 int registerNativeTgNetFunctions(JavaVM *vm, JNIEnv *env);
 int videoOnJNILoad(JavaVM *vm, JNIEnv *env);
@@ -23,10 +22,6 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved) {
 	if ((*vm)->GetEnv(vm, (void **) &env, JNI_VERSION_1_6) != JNI_OK) {
 		return -1;
 	}
-
-    if (verifySign(env) != JNI_OK) {
-        return JNI_ERR;
-    }
 
     if (imageOnJNILoad(vm, env) != JNI_TRUE) {
         return -1;
